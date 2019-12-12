7d6ee906a63fd464821fca2d0db346486bf020ae
thethongngu <nguyenthethong1996@gmail.com>
2019-12-12 12:01:00 +0800

    add gp

--- ML-hw05/gaussian_process.py
+++ ML-hw05/gaussian_process.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 beta = 5
 
 
-def kernel(X, Y):
-    pass
+def kernel(X, Y, variance, a, l):
+    return variance * (1 + (np.subtract(X, Y) ** 2) / (2 * a * l)) ** -a
 
 
 # Data (34 x 2)
@@ -14,6 +15,7 @@ raw_data = [e.split(' ') for e in raw_data.split('\n')]
 data = []
 for e in raw_data[:-1]:
     data.append([float(e[0]), float(e[1])])
+n = len(data)
 
 # C (34 x 34)
 C = np.zeros((34, 34), dtype=float)
@@ -21,4 +23,12 @@ for i in range(34):
     for j in range(34):
         C[i][j] = kernel(data[i], data[j]) + beta ** (-1)
 
-# C = np.array()
+fig, axs = plt.subplots(1, 1)
+
+graph01 = axs[0]
+graph01.set_title('Gaussian Process')
+
+x = [data[i][0] for i in range(n)]
+y = [data[i][1] for i in range(n)]
+graph01.scatter(x, y, color='red')
+
