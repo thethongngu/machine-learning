2ef9032e1cca4ae635926a97db97f306232ad7d2
Thong The Nguyen <nguyenthethong1996@gmail.com>
2019-12-12 22:05:31 +0800

    add gp

--- ML-hw05/gaussian_process.py
+++ ML-hw05/gaussian_process.py
@@ -1,34 +1,97 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
+from scipy.optimize import minimize
 
 beta = 5
+alpha = 1.0
+length = 1.0
+variance = 1.0
 
 
-def kernel(X, Y, variance, a, l):
-    return variance * (1 + (np.subtract(X, Y) ** 2) / (2 * a * l)) ** -a
+def kernel(x, y, variance, alpha, length):
+    return variance * ((1 + cdist(x, y, 'sqeuclidean') / (2 * alpha * length)) ** -alpha)
 
 
-# Data (34 x 2)
+# Read data
 f = open("input.data", "r")
 raw_data = f.read()
 raw_data = [e.split(' ') for e in raw_data.split('\n')]
-data = []
-for e in raw_data[:-1]:
-    data.append([float(e[0]), float(e[1])])
-n = len(data)
-
-# C (34 x 34)
-C = np.zeros((34, 34), dtype=float)
+train_x = np.zeros((34, 1))
+train_y = np.zeros((34, 1))
 for i in range(34):
-    for j in range(34):
-        C[i][j] = kernel(data[i], data[j]) + beta ** (-1)
+    train_x[i] = float(raw_data[i][0])
+    train_y[i] = float(raw_data[i][1])
 
+# GP
+mu = np.zeros((34, 2))
+C = kernel(train_x, train_x, variance, alpha, length) + (1.0 / beta)  # (34, 34)
+
+# GP predict
+C_inv = np.linalg.inv(C)
+test_x = np.linspace(-60, 60, 200).reshape(-1, 1)  # (200, 1)
+
+k_x_xstar = kernel(train_x, test_x, variance, alpha, length)  # (34, 200)
+k_star = kernel(test_x, test_x, variance, alpha, length) + (1.0 / beta)  # (200, 200)
+
+mu_y = np.matmul(np.matmul(k_x_xstar.T, C_inv), train_y)  # (200, 34) (34, 34) (34, 1) = (200, 1)
+variance_y = np.subtract(k_star, np.matmul(np.matmul(k_x_xstar.T, C_inv), k_x_xstar))  # (200. 200)
+upper_y = mu_y + 2 * (np.diag(np.sqrt(variance_y)).reshape(-1, 1))
+lower_y = mu_y - 2 * (np.diag(np.sqrt(variance_y)).reshape(-1, 1))
+
+# Draw graph
 fig, axs = plt.subplots(1, 1)
+axs.set_title('Gaussian Process')
+axs.set_xlim(-60.0, 60.0)
+axs.plot(test_x.ravel(), mu_y.ravel(), color='blue')
+axs.fill_between(test_x.ravel(), upper_y.ravel(), lower_y.ravel(), facecolor='pink')
+axs.plot(test_x.ravel(), upper_y.ravel(), color='red')
+axs.plot(test_x.ravel(), lower_y.ravel(), color='red')
+axs.scatter(train_x, train_y, color='black')
 
-graph01 = axs[0]
-graph01.set_title('Gaussian Process')
+fig.show()
 
-x = [data[i][0] for i in range(n)]
-y = [data[i][1] for i in range(n)]
-graph01.scatter(x, y, color='red')
 
+def loss_func(params):
+    print(params)
+    cov = kernel(train_x, train_x, params[0], params[1], params[2]) + (1.0 / beta)  # (34, 34)
+    # (34, 34)
+    # (1, 34) (34, 34) (34, 1)
+    # 1
+    res = 0.5 * np.log(np.linalg.det(cov)) + \
+          0.5 * np.matmul(np.matmul(train_y.T, np.linalg.inv(cov)), train_y) + \
+          0.5 * 34 * np.log(2 * np.pi)
+    return res[0][0]
+
+
+# Optimize parameters
+result = minimize(fun=loss_func, x0=np.array([variance, alpha, length]))
+variance = result.x[0]
+alpha = result.x[1]
+length = result.x[2]
+
+# GP
+C = kernel(train_x, train_x, variance, alpha, length) + (1.0 / beta)  # (34, 34)
+
+# GP predict
+C_inv = np.linalg.inv(C)
+
+k_x_xstar = kernel(train_x, test_x, variance, alpha, length)  # (34, 200)
+k_star = kernel(test_x, test_x, variance, alpha, length) + (1.0 / beta)  # (200, 200)
+
+mu_y = np.matmul(np.matmul(k_x_xstar.T, C_inv), train_y)  # (200, 34) (34, 34) (34, 1) = (200, 1)
+variance_y = np.subtract(k_star, np.matmul(np.matmul(k_x_xstar.T, C_inv), k_x_xstar))  # (200. 200)
+upper_y = mu_y + 2 * (np.diag(np.sqrt(variance_y)).reshape(-1, 1))
+lower_y = mu_y - 2 * (np.diag(np.sqrt(variance_y)).reshape(-1, 1))
+
+# Draw graph
+fig, axs = plt.subplots(1, 1)
+axs.set_title('Gaussian Process')
+axs.set_xlim(-60.0, 60.0)
+axs.plot(test_x.ravel(), mu_y.ravel(), color='blue')
+axs.fill_between(test_x.ravel(), upper_y.ravel(), lower_y.ravel(), facecolor='pink')
+axs.plot(test_x.ravel(), upper_y.ravel(), color='red')
+axs.plot(test_x.ravel(), lower_y.ravel(), color='red')
+axs.scatter(train_x, train_y, color='black')
+
+fig.show()
